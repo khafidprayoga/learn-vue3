d63d84d745b2151d238f043e4cd7814438d563d1
Khafid Prayoga <khafidp@pm.me>
2025-04-12 16:10:26 +0700

    learn basic state
    - ref for primitive with .value return type
    - reactive for object or complex data
    - computed untuk aggregat data dari state lain (read only)
    - writeable computed -> computed with setter that intended not modify the computed or variable itself, but modify parent state (and after parent changed, the computed will trigger re render)

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,76 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive, computed } from 'vue'
+const state = reactive({
+  authorName: 'Jane Doe',
+  books: [
+    {
+      id: 1,
+      title: 'Book World',
+      description: 'Description 1',
+    },
+    {
+      id: 2,
+      title: 'Book Heaven',
+      description: 'Description 2',
+    },
+    {
+      id: 3,
+      title: 'Book at the Park',
+      description: 'Description 3',
+    },
+  ],
+})
+
+const hasBooks = computed(() => {
+  return state.books.length > 0
+})
+
+const deleteBook = (bookId) => {
+  state.books = state.books.filter((book) => book.id !== bookId)
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <header>
+      <p>Author Name: {{ state.authorName }}</p>
+    </header>
+    <div class="content">
+      <template v-if="hasBooks">
+        <p>Book List:</p>
+        <ul>
+          <li v-for="book in state.books" :key="book.id">
+            {{ book.title }}
+            <button class="delete-button" @click="deleteBook(book.id)">-</button>
+          </li>
+        </ul>
+      </template>
+      <template v-else>
+        <p>No books available</p>
+      </template>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.container {
+  display: flex;
+  flex-direction: column;
+  width: 720px;
+  height: 25vh;
+  justify-content: center;
+  align-items: flex-start;
+  border: 1px solid #000;
+  margin: 0 auto;
+  padding: 20px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.delete-button {
+  margin-left: 10px;
+  padding: 1px 8px;
+  background-color: #ff4444;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
 }
 </style>
