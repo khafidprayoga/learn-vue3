8fe792f1d9daf0f3abbd321fddc802faa8938216
Khafid Prayoga <khafidp@pm.me>
2025-04-15 22:25:11 +0700

    todo list

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,39 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <h1 class="text-3xl font-bold">Hello World</h1>
+  <h1 class="text-2xl font-bold text-center mt-5">Todo List APP</h1>
+  <div class="container">
+    <main class="todo-list">
+      <div class="todo-item">
+        <div class="todo-content">Belajar Vue3</div>
+        <div class="todo-action">
+          <button class="btn-edit">Edit</button>
+          <button class="btn-delete">Done</button>
+        </div>
+      </div>
+      <div class="todo-item">
+        <div class="todo-content">Belajar Laravel 12 dan PostgreSQL</div>
+        <div class="todo-action">
+          <button class="btn-edit">Edit</button>
+          <button class="btn-delete">Done</button>
+        </div>
+      </div>
+      <div class="todo-item">
+        <div class="todo-content">Belajar InertiaJS</div>
+        <div class="todo-action">
+          <button class="btn-edit">Edit</button>
+          <button class="btn-delete">Done</button>
+        </div>
+      </div>
+    </main>
+    <div class="todo-form"></div>
   </div>
 </template>
 
-<style scoped></style>
+<style>
+@reference "tailwindcss";
+
+.container {
+  @apply bg-zinc-300;
+}
+</style>
